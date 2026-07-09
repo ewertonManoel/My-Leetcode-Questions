726b1f2db92da8f540e7396d6ae84583bddc19c3
Ewerton Manoel Fernandes Lira de Carvalho <ewertonfernandeslira@gmail.com>
2026-07-08 21:39:15 -0300

    feat: add solution for Tree-Node.sql

---
+++ Tree-Node.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT id,
+CASE 
+WHEN p_id IS NULL THEN 'Root'
+WHEN id IN(SELECT p_id FROM Tree WHERE p_id IS NOT NULL) THEN 'Inner'
+ELSE 'Leaf' 
+END
+as type
+FROM Tree;
+
+
